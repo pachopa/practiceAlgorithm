4ecbde5ac80ed1597db3d242eb6b6ae95277faba
chris <pachopa0320@gmail.com>
2017-07-01 08:08:48 +0000

    removed dummy code

--- digitPowerSum.js
+++ digitPowerSum.js
@@ -1,11 +1,4 @@
 
-/*
-  1. 겹치지 않는 숫자 길이가 제곱 숫자
-  2. 한 자리 숫자를 각각 더한 숫자가  base number
-  3. 제곱 숫자를 연산 후, 그 대상의 숫자와 숫자가 같은 값
-  4. a = b^c
-  5. a2= 512, a4 = 614656, a5 = ? // 정답  a5 : 1679616
-*/
 function findAnswer() {
   for(var inputNum = 614657;  ;inputNum++) {
     var input = inputNum;
