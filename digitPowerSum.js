c7c192e059942371297b0040e9793d5e41a7944b
chris <pachopa0320@gmail.com>
2017-07-04 17:17:39 +0000

    attached second version for result and better readability and clean

--- digitPowerSum.js
+++ digitPowerSum.js
@@ -37,3 +37,75 @@ function findOutput(result, sum, input) {
 console.log(findAnswer())
 
 
+filter를 다시 생각해보기 -1로 대체
+
+
+------------------------//Second Version
+
+
+
+function sepNsum(n){
+  /*  Convert int to array
+      last array value is sum of each digits of n
+      512 -> [2,1,5,8]
+  */
+  var ten = 10, remain = 0
+  var store = []
+  while(n != 0){
+    remain = n % ten
+    store.push(remain)
+    n = parseInt(n / ten)
+  }
+  store.push(store.reduce(add,0))
+  return store
+}
+
+function add(a,b){
+  // callback function
+  return a + b
+}
+
+function powerLen(arry){
+  /*  Replace Duplicate Value with -1
+      Only greater than -1 is count
+      [1,-1,2,-1,-1,5] -> 3
+
+  */
+  var count = 0
+  var store = []
+
+  arry = arry.slice(0,arry.length - 1)
+
+  for(var i = 0; i < arry.length; i++){
+    for(var j = i + 1; j < arry.length; j++){
+      if(arry[i] == arry[j]){
+        arry[j] = -1
+      }
+    }
+  }
+
+  for(var i = 0; i < arry.length; i++){
+    if(arry[i] != -1){
+      count += 1
+    }
+  }
+
+  return count
+
+}
+
+
+var info, base,power
+
+console.log(Math.pow(base,power))
+
+for(var i = 1; i < 10000000; i++){
+    info = sepNsum(i)
+    base = info[info.length-1]
+    power = powerLen(info)
+
+    if(i == Math.pow(base, power)){
+      console.log(i)
+    }
+
+}
