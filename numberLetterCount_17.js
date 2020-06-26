b3ceae69a305e09c025e8c6188c2de72aeb24910
Chris Lee <chrislee910320@gmail.com>
2020-06-25 19:56:01 -0600

    Still work on solving the question

--- numberLetterCount_17.js
+++ numberLetterCount_17.js
@@ -1,5 +1,7 @@
 
 /*
+
+answer : 21124 
   Number letter counts
 Show HTML problem content
 Problem 17
@@ -11,13 +13,75 @@ If all the numbers from 1 to 1000 (one thousand) inclusive were written out in w
 NOTE: Do not count spaces or hyphens. For example, 342 (three hundred and forty-two) contains 23 letters and 115 (one hundred and fifteen)
 contains 20 letters. The use of "and" when writing out numbers is in compliance with British usage.
 
-
 */
 
-/* 
-one two three four five six seven eight nine ten
-3    3   5    4     4 
+let oneToNine = 36;
+let tenToNineTeen = 62;
+let twentyToNinety = 47;
 
+
+console.log(twentyToNinety)
+
+
+
+/* formula until 100
+
+one to nine * 9 * 10 * 
+
+ten to nineteen * 10;
+
+twentytoninety * 1000;
+
+hundred: 7 * 90 * 10 / one thousand 11
+
+
+/////////////////////////////////
+1 ~ 9
+
+one to nine 36 ^^^^^ 36
+
+
+10 ~ 99
+
+1 ~ 9 * 8 / 10 ~ 19 / 20 ~ 90 * 10
+(36)        (62)        (47)
+
+
+100 ~ 999
+1~ 9 * 90 * 1000 / 10 ~ 19 * 10 / 20 ~ 90 * 8 * 10 / 7 * 100 * 10
+(36)                (62)            (47)    
+
+1000
+
+one thousand 11 
+ 
+*/
+
+
+/* Reference 
+one two three four five six seven eight nine 
+3    3   5    4     4   3    5     5     4    
+
+ten eleven twelve thirteen fifteen sixteen seventeen eighteen nineteen 
+ 3   6         6       8     7        7        9         8      8
+twenty 
+6
+thirty
+6
+fourty 
+6
+fifty 
+5
+sixty
+5
+seventy
+7
+eighty
+6
+ninety
+6
+
+one hundred 
 
 */
 
