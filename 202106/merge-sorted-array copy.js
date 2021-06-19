d841bc179ee5d86b16defbf13a8146e8d71cf2c8
Daechul <dlee@nuundigital.com>
2021-06-19 12:57:21 -0600

    #2 merged sorted array

--- 202106/merge-sorted-array copy.js
+++ 202106/merge-sorted-array copy.js
@@ -33,37 +33,68 @@
 //  * @return {void} Do not return anything, modify nums1 in-place instead.
 //  */
 //  var merge = function(nums1, m, nums2, n) {
+//     if(m === 0) {
+//         nums1[m] = nums2[n - 1];
+//         return nums1
+//     }
+//     let a = m + n - 1;
+//     m--;
+//     n--;
 
+//     while (m > -1 && n > -1) {
+//         if (nums2[n] > nums1[m]) {
+//             nums1[a] = nums2[n];
+//             n--;
+//         } else if (nums2[n] < nums1[m]) {
+//             nums1[a] = nums1[m];
+//             nums1[a - 1] = nums2[n];
+//             m--;
+//             n--;
+//         } else {
+//             break;
+//         }
+//         a--;
+//     }
 // };
 
+
 let nums1 = [1, 2, 3, 0, 0, 0], m = 3, nums2 = [2, 5, 6], n = 3;
+//          2,5,6
+// let nums1 = [4, 5, 6, 0, 0, 0], m = 3, nums2 = [1, 2, 3], n = 3;
+//          1, 2, 3
 
 function test(nums1, m, nums2, n) {
-    // for (let  = 0;  < array.length; ++) {
-    //     const element = array[];
+    if (m === 0) {
+        nums1[m] = nums2[n - 1];
+        return nums1;
+    }
+    let a = m + n - 1;
+    m--;
+    n--;
+    console.log("a, m,n", a, m, n);
 
-    // // }
-    // for (let i = 0; i < m + n; i++) {
-    //     // const element = array[i];
-
-    m -= 1;
-    n -= 1;
-    console.log(m, n);
-
-    while (m > -1 && n > -1) {
+    while (a > -1) {
         if (nums2[n] > nums1[m]) {
-            nums1[m + n] = nums2[n];
+            nums1[a] = nums2[n];
             console.log(nums1);
-
             n--;
         } else if (nums2[n] < nums1[m]) {
-            m--;
-        } else {
+            nums1[a] = nums1[m];
+            nums1[a - 1] = nums2[n];
+            console.log(nums1);
             m--;
             n--;
+        } else {
+            break;
         }
+        a--;
     }
     console.log(nums1);
 }
 
-test(nums1, m, nums2, n);
+
+
+
+
+
+console.log(test(nums1, m, nums2, n), typeof test(nums1, m, nums2, n));
