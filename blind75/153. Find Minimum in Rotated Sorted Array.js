cdda928552dbc6081165874f7ccc3e5dd879b4ef
chrislee.dc.developer <chrislee.dc.developer@gmail.com>
2024-11-22 05:52:20 +0000

    commit

---
+++ blind75/153. Find Minimum in Rotated Sorted Array.js
@@ -0,0 +1,71 @@
+console.clear();
+
+// Suppose an array of length n sorted in ascending order is rotated between 1 and n times. For example, 
+// the array nums = [0,1,2,4,5,6,7] might become:
+
+// [4,5,6,7,0,1,2] if it was rotated 4 times.
+// [0,1,2,4,5,6,7] if it was rotated 7 times.
+// Notice that rotating an array [a[0], a[1], a[2], ..., a[n-1]] 1 time results in the array [a[n-1], a[0], a[1], a[2], ..., a[n-2]].
+
+// Given the sorted rotated array nums of unique elements, return the minimum element of this array.
+
+// You must write an algorithm that runs in O(log n) time.
+
+ 
+
+// Example 1:
+
+// Input: nums = [3,4,5,1,2]
+// Output: 1
+// Explanation: The original array was [1,2,3,4,5] rotated 3 times.
+
+// Example 2:
+// Input: nums = [4,5,6,7,0,1,2]
+// Output: 0
+// Explanation: The original array was [0,1,2,4,5,6,7] and it was rotated 4 times.
+
+// Example 3:
+// Input: nums = [11,13,15,17]
+// Output: 11
+// Explanation: The original array was [11,13,15,17] and it was rotated 4 times. 
+
+
+
+/**
+ * LEET CODE ANSWER
+ */
+var findMin2 = function(nums) {
+    const n=nums.length;
+    let start=0,end=n-1;
+    let min=Number.MAX_SAFE_INTEGER;
+    while(start<=end){
+     const mid=start+Math.floor((end-start)/2);
+     if(nums[start]<=nums[mid]){
+        min=Math.min(nums[start],min);
+        start=mid+1;
+     }
+     else{
+        min=Math.min(nums[mid],min);
+        end=mid-1;
+     }
+    }
+    return min;
+};
+
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMin = function(nums) {
+    return Math.min(...nums);
+};
+
+
+const result = [3,4,5,1,2]
+const result2 = [4,5,6,7,0,1,2]
+const result3 = [11,13,15,17]
+
+
+console.log(findMin(result))
+
